d28383ee395179c3f2ede73c1a06d8a10fe93456
Ronald Magno <dlanoronald123@gmail.com>
2021-10-14 17:36:09 +0800

    Starting with components

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  // styleUrls: ['./server.component.css']
+  styles: [
+  `
+  p{
+  padding: 20px;
+  border: 1px solid red;
+  background-color: mistyrose;
+  }
+  `]
+})
+
+export class WarningAlertComponent {
+
+}
